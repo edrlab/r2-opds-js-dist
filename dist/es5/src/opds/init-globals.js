e098bb44c24c2505a8c6e0f68fa5d7d014434ac3
danielweck <daniel.weck@gmail.com>
2018-08-30 00:25:49 +0100

    add new dist

--- dist/es5/src/opds/init-globals.js
+++ dist/es5/src/opds/init-globals.js
@@ -1,6 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var ta_json_date_converter_1 = require("r2-utils-js/dist/es5/src/_utils/ta-json-date-converter");
+var ta_json_number_converter_1 = require("r2-utils-js/dist/es5/src/_utils/ta-json-number-converter");
 var xml_js_mapper_1 = require("r2-utils-js/dist/es5/src/_utils/xml-js-mapper");
 var ta_json_1 = require("ta-json");
 var opds2_collection_1 = require("./opds2/opds2-collection");
@@ -12,6 +13,7 @@ exports.initGlobalConverters_OPDS = initGlobalConverters_OPDS;
 function initGlobalConverters_GENERIC() {
     ta_json_1.propertyConverters.set(Buffer, new ta_json_1.BufferConverter());
     ta_json_1.propertyConverters.set(Date, new ta_json_date_converter_1.JsonDateConverter());
+    ta_json_1.propertyConverters.set(Number, new ta_json_number_converter_1.JsonNumberConverter());
     xml_js_mapper_1.propertyConverters.set(Buffer, new xml_js_mapper_1.BufferConverter());
     xml_js_mapper_1.propertyConverters.set(Date, new xml_js_mapper_1.DateConverter());
 }
